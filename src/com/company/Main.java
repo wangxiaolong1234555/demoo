2d03853b28a3987fdaad6bd9f173f71f1bf4fff8
wangxiaolong <3355137186@qq.com>
2021-05-17 08:47:59 +0800

    课堂笔记

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Buffer
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader=new BufferedReader(new FileReader("C:\\Users\\wangxiaolong\\Desktop\\you-get 下载.txt"));
+        BufferedReader writer=new BufferedWriter(new FileWriter("D:\\des\\hah.txt"));
+        int  len=0;
+        char [] buffer=new char[1024];
+        long start=System.currentTimeMillis();
+        while (len=reader.read(buffer))!=-1){
+            writer.write(buffer,0,len);
+        }
+        writer.flush();
+        long end=System.currentTimeMillis();
+        System.out.println("时间："+(end-start));
+        reader.close();
+        writer.close();
     }
 }
